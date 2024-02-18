04e520498d1362efddad54844ea8c3aaa7f3d7d5
echo724 <echo.backend@gmail.com>
2024-02-18 22:49:04 +0900

    chore: filebeat 및 로그백 설정

---
+++ user/src/main/kotlin/org/mungchi/user/TestController.kt
@@ -0,0 +1,13 @@
+package org.mungchi.user
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "Hello, World!"
+    }
+}
