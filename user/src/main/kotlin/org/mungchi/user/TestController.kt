055d05c51fcf9b17dfb6ba2da0d7d5ad9432e472
echo724 <echo.backend@gmail.com>
2024-02-19 16:06:02 +0900

    chore: Loggger 추가 및 filebeat input 수정

--- user/src/main/kotlin/org/mungchi/user/TestController.kt
+++ user/src/main/kotlin/org/mungchi/user/TestController.kt
@@ -1,13 +1,15 @@
 package org.mungchi.user
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-
+import java.util.logging.Logger
 @RestController
 class TestController {
-
+    val logger = LoggerFactory.getLogger(this.javaClass)
     @GetMapping("/test")
     fun test(): String {
+        logger.info("test")
         return "Hello, World!"
     }
 }
